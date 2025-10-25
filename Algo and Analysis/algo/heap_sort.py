3a4eefcc981b9a1cb8d94bf6ae167eb56f65cf2d
Acrylic125 <acrylic125email@gmail.com>
2025-10-25 10:43:36 +0800

    heap sort

--- Algo and Analysis/algo/heap_sort.py
+++ Algo and Analysis/algo/heap_sort.py
@@ -30,13 +30,30 @@ def heapify(l, i):
 
     fix_heap(l, i)
 
+def push_to_heap(l, k):
+    l.append(k)
+    heapify(l, 1)
+
+def pop_heap(l):
+    popped = l.pop(0)
+    heapify(l, 1)
+    return popped
+
 l = [7, 3, 1, 5, 6, 3, 0, -3, 7]
 print(f"Sorting {l}")
 heapify(l, 1)
 print(f"Heapified {l}")
+while len(l) > 0:
+    print(pop_heap(l))
 
 l = [8, 7, 6, 5, 4, 3, 2, 1]
 print(f"Sorting {l}")
 heapify(l, 1)
 print(f"Heapified {l}")
+push_to_heap(l, 3)
+print(f"After pushing 3: {l}")
+push_to_heap(l, -10)
+print(f"After pushing -10: {l}")
 
+while len(l) > 0:
+    print(pop_heap(l))
